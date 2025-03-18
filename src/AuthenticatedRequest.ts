ceca6805c9bef2101f76d60b3aab6ee54930152b
Zefir Kirilov <contact@zefir.pro>
2025-03-18 11:28:16 +0200

    add typedoc comments on AuthenticatedRequest

--- src/AuthenticatedRequest.ts
+++ src/AuthenticatedRequest.ts
@@ -1,9 +1,22 @@
 import {Authorisation} from "./auth/Authorisation.js";
 import {Permissible} from "./auth/Permissible.js";
+import {Permission} from "./auth/Permission.js";
 import {Request} from "./Request.js";
 
+/**
+ * A request with available {@link Authorisation}.
+ */
 export class AuthenticatedRequest<A> extends Request<A> implements Permissible {
+    /**
+     * This request’s authorisation.
+     */
     public readonly authorisation: Authorisation<A>;
+
+    /**
+     * Create a new authenticated request.
+     * @param authorisation
+     * @param args The arguments to pass to the {@link Request} constructor.
+     */
     public constructor(
         authorisation: Authorisation<A>,
         ...args: ConstructorParameters<typeof Request<A>>
