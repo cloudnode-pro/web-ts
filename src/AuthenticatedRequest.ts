a67b93badfe2a118b47467f5b842610c459b1094
Zefir Kirilov <contact@zefir.pro>
2025-03-18 11:28:02 +0200

    implement Permissible on AuthenticatedRequest

--- src/AuthenticatedRequest.ts
+++ src/AuthenticatedRequest.ts
@@ -1,7 +1,8 @@
 import {Authorisation} from "./auth/Authorisation.js";
+import {Permissible} from "./auth/Permissible.js";
 import {Request} from "./Request.js";
 
-export class AuthenticatedRequest<A> extends Request<A> {
+export class AuthenticatedRequest<A> extends Request<A> implements Permissible {
     public readonly authorisation: Authorisation<A>;
     public constructor(
         authorisation: Authorisation<A>,
@@ -9,5 +10,13 @@ export class AuthenticatedRequest<A> extends Request<A> {
     ) {
         super(...args);
         this.authorisation = authorisation;
+    }
+
+    /**
+     * Check if the request has the specified permission.
+     * @param permission
+     */
+    public has(permission: Permission): boolean {
+        return this.authorisation.has(permission);
     }
 }
